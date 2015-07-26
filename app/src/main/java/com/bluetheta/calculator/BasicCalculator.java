a986b0a5585853967063f51ec2e7dc11d25bcc15
jeffbooth <boothsemailaddress@gmail.com>
2015-07-25 18:33:51 -0700

    Action listeners added to most operator buttons. TODOs added where needed

--- app/src/main/java/com/bluetheta/calculator/BasicCalculator.java
+++ app/src/main/java/com/bluetheta/calculator/BasicCalculator.java
@@ -10,7 +10,7 @@
 
 package com.bluetheta.calculator;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class BasicCalculator extends ActionBarActivity {
+public class BasicCalculator extends Activity {
 
     // TextView holds the display view
     private TextView display;
